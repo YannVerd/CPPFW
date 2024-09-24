6add379d32774d5a47324bc805ea4e298220d56c
YannV <yann.e.verdier@gmail.com>
2024-09-24 19:19:28 +0200

    test for handle arrow inoput before implements and add menu's class

--- terminal.cpp
+++ terminal.cpp
@@ -7,19 +7,37 @@ int main(void)
 {	
 
    WINDOW *titleBox, *menuBox;
-    
+    const int startXCursor = 5;
+    const int startYCursor = 3;
     initscr();
+    keypad(stdscr, TRUE);
     titleBox= subwin(stdscr, 3, COLS, 0, 0 );
     menuBox= subwin(stdscr, 8, COLS, 4, 0);
-   
     box(titleBox, ACS_VLINE, ACS_HLINE); // ACS_VLINE et ACS_HLINE sont des constantes qui génèrent des bordures par défaut
     box(menuBox, ACS_VLINE, ACS_HLINE);
-  
+    move(startXCursor, startYCursor);
+
+   
     mvwprintw(titleBox, 1, 1, "CPPFW - Bienvenue dans le framework cpp pour HTML/CSS");
     mvwprintw(menuBox, 0, 2, "Sélectionner vos options");
     wrefresh(titleBox);
     wrefresh(menuBox);
-    getch();
+    
+
+    while(1){
+        int c = getch();
+        move(startXCursor, startYCursor);
+        clrtoeol();
+        if(c == KEY_UP){
+            printw("key up pressed");
+        }
+        if(c == KEY_DOWN){
+            printw("key down pressed");
+        }
+        
+        wrefresh(menuBox);
+    }
+
     endwin();
     
     free(titleBox);
